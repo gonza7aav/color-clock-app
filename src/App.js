1804da99ed44755de9dcbd8cf26647764cb94041
Aguirre Gonzalo Adolfo <gonza.a.aguirre@gmail.com>
2021-07-16 02:52:03 -0300

    V2.1.0 (#1)
    * uncommited changes
    
    * v2.1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { useState, useEffect } from "react";
-import Clock from "./components/Clock";
+import { useState, useEffect } from 'react';
+import Clock from './components/Clock';
 
-const getTime = (date = new Date()) => {
+const getTime = () => {
+  let date = new Date();
   return {
     hours: date.getHours(),
     minutes: date.getMinutes(),
     seconds: date.getSeconds(),
+    // milliseconds: date.getMilliseconds(),
   };
-};
-
-const changeBackground = ({ red, green, blue }) => {
-  document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
 };
 
 const App = () => {
   const [time, setTime] = useState(getTime());
 
-  const update = () => {
+  const changeBackground = ({ red, green, blue }) => {
+    document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+  };
+
+  const updateTime = () => {
     let now = getTime();
     setTime(now);
-
     changeBackground({
       red: now.hours,
       green: now.minutes,
@@ -28,20 +29,25 @@ const App = () => {
   };
 
   useEffect(() => {
+    // right after loading everything, update the clock
+    updateTime();
+
+    // the timeout will be executed in the beginning of the next second
     let now = new Date();
     let ms = now.getMilliseconds();
-
-    // with this the interval start at 0 milliseconds
     setTimeout(() => {
+      updateTime();
+
+      // the interval will be executed every second passed
       setInterval(() => {
-        update();
+        updateTime();
       }, 1000);
     }, 1000 - ms);
   }, []);
 
   return (
-    <div className="center">
-      <Clock Time={time} />
+    <div className='center'>
+      <Clock time={time} />
     </div>
   );
 };
--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -3,14 +3,19 @@ const formatNumber = (n) => {
   return `${n}`;
 };
 
-const Clock = ({ Time }) => {
-  const { hours: h, minutes: m, seconds: s } = Time
+const Clock = ({ time }) => {
+  const { hours: h, minutes: m, seconds: s } = time;
+
+  const style = {
+    padding: '0.25rem 0.5rem',
+    fontSize: '10vw',
+  };
 
   return (
-    <p className='Clock'>
+    <p style={style}>
       {`${formatNumber(h)}:${formatNumber(m)}:${formatNumber(s)}`}
     </p>
-  )
-}
+  );
+};
 
-export default Clock
+export default Clock;
